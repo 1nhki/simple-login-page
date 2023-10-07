e1a4635e40c0e0de44231a07252887ab67bcc549
1nhki <inhki@outlook.co.id>
2023-10-07 16:53:08 +0700

    new file:   .vscode/c_cpp_properties.json 	new file:   .vscode/launch.json 	new file:   .vscode/tasks.json 	new file:   Debug/login.exe 	modified:   main.cpp

---
+++ main.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <string>
+#include <windows.h>
+using namespace std;
+
+int main(){
+    int a {0};
+    int pass;
+    int att {20};
+    do{
+        do{
+            cout << "\nEnter password: ";
+            cin >> pass;
+            a++;
+        }while (pass != 12345 && a < 3);
+
+        if (a == 3){
+            int time = att;
+            cout << "you have wrong 3 times" << "\nyou need wait " << time << " seconds\n";
+            for (int i = time ; i >= 0; i--){
+                system ("cls");
+                cout << "\n========\n" << i << "seconds\n" << "========";
+                Sleep(1000);
+                
+            }
+            att *= 2;
+        }
+        a = 0;
+    }while (pass != 12345);
+
+    cout << "Password is valid !!\n";
+    system ("pause");
+        
+    
+
+
+    
+    
+
+
+
+
+
+
+
+    return 0;
+}
